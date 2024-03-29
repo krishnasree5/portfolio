9608c70e9b0fff6af4b54a408d76fb34eed1560c
{Krishna <gkrishnasree5@gmail.com>
2024-03-29 14:30:36 +0530

    Redesign

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function scrollConnect(){
-    const button = document.querySelector('#connect-btn');
-    const section = document.querySelector('#connect');
-    window.scrollTo({
-        top: section.offsetTop,
-        behavior: 'smooth',
-    });
-};
